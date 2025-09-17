6877178831558eb5ab1765c46907d77afa310e73
Server <161137675+Gimkit0@users.noreply.github.com>
2025-09-16 23:43:28 -0500

    Update time_bypass_test.js

--- time_bypass_test.js
+++ time_bypass_test.js
@@ -1,19 +1,16 @@
 function getNewID() {
-    var d = new Date().getTime();
-    if (window.performance && typeof window.performance.now === "function") {
-        d += performance.now();
+    let hex = "";
+    for (let i = 0; i < 32; i++) {
+        hex += Math.floor(Math.random() * 16).toString(16);
     }
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random() * 16) % 16 | 0;
-        d = Math.floor(d / 16);
-        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
-    return uuid;
+    return hex;
 }
+
 function newFingerprint() {
     localStorage.setItem('EASYFUN_FINGER_PRINT', getNewID());
     localStorage.setItem('EASYFUN_LOGIN_SOURCE', 3);
 }
+
 function getLocalStorageData() {
     Object.keys(localStorage).forEach(key => {
         console.log(`${key}: ${localStorage.getItem(key)}`);
