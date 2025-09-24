1b45a27f5991cdbe781b431d33c60b2b0e1b8f44
Server <161137675+Gimkit0@users.noreply.github.com>
2025-09-23 21:26:03 -0500

    Update 7016.dac99e5d2d50a0fc.js

--- easyfun/static/chunks/7016.dac99e5d2d50a0fc.js
+++ easyfun/static/chunks/7016.dac99e5d2d50a0fc.js
@@ -86,11 +86,11 @@
               , t = e("play-now")
               , r = e("start-play")
               , s = e("visitor-limit-time", {
-                min: 999999
+                min: 30
             })
               , p = e("login")
               , g = e("login-limit-time", {
-                hour: 999999
+                hour: 6
             })
               , v = (0,
             n.useAtomValue)(d.Zm)
