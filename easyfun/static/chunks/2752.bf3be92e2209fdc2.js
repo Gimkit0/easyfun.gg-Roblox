0756e1d719a3ffbe874f8f9e31507df209bae27a
Server <161137675+Gimkit0@users.noreply.github.com>
2025-09-24 00:26:46 -0500

    Update 2752.bf3be92e2209fdc2.js

--- easyfun/static/chunks/2752.bf3be92e2209fdc2.js
+++ easyfun/static/chunks/2752.bf3be92e2209fdc2.js
@@ -2629,14 +2629,14 @@
             o.yF)( (t, e, o) => {
                 var l;
                 let {wss: p} = t(n.Lp);
-                o.remainingTime = 9999999
+                o.remainingTime = 999999999
                 if (2 === o.connectStatus && (null == (l = p.getWSInstance()) ? void 0 : l.readyState) === WebSocket.OPEN) {
                     console.log("设备重连 并且 设备wss 正在链接中，不需要重连"),
-                    e(a.Bp, o.remainingTime);
+                    //e(a.Bp, o.remainingTime);
                     return
                 }
                 o.deviceId && o.phoneId && o.accessPort && o.publicIp ? (console.log("同步用户时间"),
-                e(a.Bp, o.remainingTime),
+                //e(a.Bp, o.remainingTime),
                 console.log("同步设备信息"),
                 s(o),
                 console.log("获取按键信息"),
